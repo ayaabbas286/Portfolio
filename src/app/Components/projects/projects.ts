096a447d912e3c112000444a87a93f7c2acd9e19
ayaabbas286 <ayaabbas286@gmail.com>
2025-12-21 12:08:17 +0200

    a

--- src/app/Components/projects/projects.ts
+++ src/app/Components/projects/projects.ts
@@ -30,7 +30,6 @@ _projects = toSignal(this.projects$, { initialValue: [] as IProjects[] });
 cardInitialCount = signal(6);
 visibleCards = computed(()=>{
   const listArray= this._projects();
-  console.log(listArray);
 return Array.isArray(listArray)?  listArray.slice(0, this.cardInitialCount()) : []} )
 LoadMore(){
   this.cardInitialCount.set(this.cardInitialCount() + 6);
