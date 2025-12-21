4ddc7df9ca7f5e0fd7578344706a81db1e6e4e92
ayaabbas286 <ayaabbas286@gmail.com>
2025-12-21 12:04:48 +0200

    @Update logic in looping cards

--- src/app/Components/projects/projects.ts
+++ src/app/Components/projects/projects.ts
@@ -26,8 +26,11 @@ export class Projects{
 
 
 _projects = toSignal(this.projects$, { initialValue: [] as IProjects[] });
+
 cardInitialCount = signal(6);
-visibleCards = computed(()=> this._projects().slice(0, this.cardInitialCount()) )
+visibleCards = computed(()=>{
+  const listArray= this._projects();
+return Array.isArray(listArray)?  listArray.slice(0, this.cardInitialCount()) : []} )
 LoadMore(){
   this.cardInitialCount.set(this.cardInitialCount() + 6);
 }
