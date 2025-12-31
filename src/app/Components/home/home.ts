d9cf08dd60ff3c5f2dc44154a2997076feb7a114
ayaabbas286 <ayaabbas286@gmail.com>
2025-12-31 11:45:16 +0200

    @final animation text

--- src/app/Components/home/home.ts
+++ src/app/Components/home/home.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterLink, RouterModule } from "@angular/router";
 import { AboutMe } from "../about-me/about-me";
 import { Services } from '../services/services';
 import { Projects } from "../projects/projects";
+import { createTimeline, splitText, stagger } from 'animejs';
+
 
 
 
@@ -12,6 +14,27 @@ import { Projects } from "../projects/projects";
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements AfterViewInit {
+ ngAfterViewInit(): void {
+
+ const jobTitle = document.getElementById('job-title')
+
+    if (!jobTitle) return;
+
+    const { chars } = splitText(jobTitle, {
+      chars: { wrap: 'clip', clone: 'bottom' }
+    });
+
+    createTimeline()
+      .add(chars, {
+        y: '-100%',
+        loop: false,
+        loopDelay: 350,
+        duration:1000,
+        ease: 'inOut(2)',
+      }, stagger(150, { from: 'center' }));
+
+
+ }
 
 }
